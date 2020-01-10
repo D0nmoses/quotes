35ba12f1f9057f27de93c00e0dbe75696fb9d0a9
Don Moses <githinjidon@gmail.com>
2020-01-10 10:30:01 +0300

    add logic for toggling quotes, adding a quote and deleting a quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,32 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
+  showDescription = false;
+
   quotes:Quote[]=[
     new Quote(1,"Freidrich Nietzsche","That which does not kill us makes us stronger","Don Moses",new Date(1879,3,21)),
     new Quote(2,"Albert Camus","Autumn is a second spring when every leaf is a flower","Maggie Ng'ethe",new Date(1939,4,22)),
     new Quote(3,"Jean-Paul Sartre","When the rich wage war it's the poor who die.","Monica Oyugi",new Date(1955,5,23))
   ];
+
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
+    deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Do you want to delete this Quote? ${this.quotes[index].name}`)//alert
+
+      if (toDelete) { this.quotes.splice(index, 1) }//removes quote
+    }
+  }
+
+  toggleQuote() {
+    this.showDescription = !this.showDescription;//show or hide content
+  }
 
   constructor() { }
 
